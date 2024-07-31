e9afa55e7bb53f2f5c6ce73006615acfd0590322
Gaurav Singh <gauravrajputsingh94@gmail.com>
2024-07-31 07:02:07 +0530

    fixed some issues.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import NavBar from "./components/NavBar/NavBar";
+import AllInBox from "./components/AllInbox/AllInbox";
 import ViewMail from "./components/ViewMail/ViewMail";
+import Sidebar from "./components/SideBar/SideBar";
+
 function App() {
   return (
-    <>
-      <div className="h-screen text-white bg-black">
-        <ViewMail />
+    <div className="h-screen w-full bg-black text-white flex flex-col">
+      <NavBar />
+      <div className="flex w-full" style={{ height: "calc(100vh - 64px)" }}>
+        <Sidebar />
+        <AllInBox />
+        <div className="flex-grow">
+          <ViewMail />
+        </div>
       </div>
-    </>
+    </div>
   );
 }
+
 export default App;
--- src/components/AllInbox/AllInbox.tsx
+++ src/components/AllInbox/AllInbox.tsx
@@ -7,7 +7,7 @@ import EmailListCard from "./EmailListCard";
 
 const AllInBox = () => {
   return (
-    <div className="bg-black w-72 flex flex-col items-center px-6 py-8">
+    <div className="bg-black w-72 flex flex-col items-center px-6 py-8 h-full border-r-[1px] border-[#FFFFFF33]">
       <div className="w-full flex items-center justify-between">
         <div className="flex justify-center items-center gap-2">
           <h1 className="text-[#4285F4] text-[20px] font-bold">All Inbox(s)</h1>
@@ -22,7 +22,7 @@ const AllInBox = () => {
 
       <div className="flex justify-start w-full">
         <div className="flex gap-2">
-          <div className="font-bold">25/25 </div>
+          <div className="font-bold">25/25</div>
           <span className="text-[#7F7F7F]">Inboxes selected</span>
         </div>
       </div>
@@ -32,7 +32,7 @@ const AllInBox = () => {
       </div>
 
       <div className="w-full mt-2 flex justify-between">
-        <div className="flex items-center gap-2 ">
+        <div className="flex items-center gap-2">
           <div>
             <ReplyCount count={26} />
           </div>
@@ -42,12 +42,12 @@ const AllInBox = () => {
         <div className="flex items-center gap-2">
           <h1 className="font-semibold">Newest</h1>
           <div>
-            <img src={DropDownIcon} alt="Drop Down Iconj" />
+            <img src={DropDownIcon} alt="Drop Down Icon" />
           </div>
         </div>
       </div>
 
-      <div className="mt-4 w-full overflow-y-auto">
+      <div className="mt-4 w-full flex-1 overflow-y-auto">
         <EmailListCard />
         <EmailListCard />
         <EmailListCard />
@@ -61,4 +61,5 @@ const AllInBox = () => {
     </div>
   );
 };
+
 export default AllInBox;
